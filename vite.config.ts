ac96b7cf1a681c504bda7170a5ac0d7d27a5594e
williamramsay557-prog <williamramsay557@gmail.com>
2025-10-29 19:13:04 +0000

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
