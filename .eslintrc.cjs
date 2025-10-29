ed1863aea3108dc04380ec03575942c7b58ff8e6
williamramsay557-prog <williamramsay557@gmail.com>
2025-10-29 21:30:51 +0000

    chore: add CI, ESLint, Prettier and proposed strict tsconfig; add scripts

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+﻿module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    // Conservative starter rules to avoid large automated churn
+    'no-console': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
